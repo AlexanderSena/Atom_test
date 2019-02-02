8055079f929ff188d28164d04e3a07130d817591
Alexander P Sena <asena@umich.edu>
2019-02-01 22:51:15 -0500

    Collects data from simple.html

--- TestScrape.py
+++ TestScrape.py
@@ -1,2 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+
+with open('simple.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+#print(soup.prettify())
+
+for article in soup.find_all('div', class_='article'):
+#print(article)
+    headline = article.h2.a.text
+    print(headline)
+
+    summary = article.p.text
+    print(summary)
+
+    print()
